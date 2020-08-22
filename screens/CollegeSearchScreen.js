4061a534806596c643db984ee87405b9c36860ef
h510-cell <58731327+h510-cell@users.noreply.github.com>
2020-08-22 21:35:18 +0530

    Add files via upload

---
+++ screens/CollegeSearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
+
+export default class CollegeSearchScreen extends React.Component{
+    render(){
+        return(
+            <View>
+             <Text>Welcome To CollegeSearchScreen</Text>   
+            </View>    
+        )
+    }
+}
